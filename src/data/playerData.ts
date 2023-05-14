fb032c70b3b904bc6af9a1ef1fe9151b16fb695e
treheinguy <treheinguy@gmail.com>
2023-05-13 21:38:28 -0500

    added player data

---
+++ src/data/playerData.ts
@@ -0,0 +1,257 @@
+export type PlayerInfo = {
+    name: string,
+    position: string,
+    number: number,
+    club: string
+}
+
+export const playerData: Array<PlayerInfo> = [
+    // City
+    {
+        name: 'Ederson',
+        position: 'G',
+        number: 18,
+        club: 'Manchester City'    
+    },
+    {
+        name: 'Kyle Walker',
+        position: 'D',
+        number: 2,
+        club: 'Manchester City'    
+    },    
+    {
+        name: 'Ruben Dias',
+        position: 'D',
+        number: 3,
+        club: 'Manchester City'    
+    },    
+    {
+        name: 'John Stones',
+        position: 'D',
+        number: 5,
+        club: 'Manchester City'    
+    },    
+    {
+        name: 'Nathan Ake',
+        position: 'D',
+        number: 6,
+        club: 'Manchester City'    
+    },    
+    {
+        name: 'Aymeric Laporte',
+        position: 'D',
+        number: 14,
+        club: 'Manchester City'    
+    },    
+    {
+        name: 'Manuel Akanji',
+        position: 'D',
+        number: 25,
+        club: 'Manchester City'    
+    },    
+    {
+        name: 'Ilkay Gundogan',
+        position: 'M',
+        number: 8,
+        club: 'Manchester City'    
+    },    
+    {
+        name: 'Jack Grealish',
+        position: 'M',
+        number: 10,
+        club: 'Manchester City'    
+    },    
+    {
+        name: 'Rodri',
+        position: 'M',
+        number: 16,
+        club: 'Manchester City'    
+    },    
+    {
+        name: 'Kevin De Bruyne',
+        position: 'M',
+        number: 17,
+        club: 'Manchester City'    
+    },    
+    {
+        name: 'Bernardo Silva',
+        position: 'M',
+        number: 20,
+        club: 'Manchester City'    
+    },    
+    {
+        name: 'Phil Foden',
+        position: 'M',
+        number: 47,
+        club: 'Manchester City'    
+    },
+    {
+        name: 'Cole Palmer',
+        position: 'M',
+        number: 80,
+        club: 'Manchester City'    
+    },
+    {
+        name: 'Erling Haaland',
+        position: 'F',
+        number: 9,
+        club: 'Manchester City'    
+    },
+    {
+        name: 'Julian Alverez',
+        position: 'F',
+        number: 19,
+        club: 'Manchester City'    
+    },
+    {
+        name: 'Riyad Mahrez',
+        position: 'F',
+        number: 26,
+        club: 'Manchester City'    
+    },
+    // Liverpool
+    {
+        name: 'Alisson',
+        position: 'G',
+        number: 1,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'Joe Gomez',
+        position: 'D',
+        number: 2,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'Virgil van Dijk',
+        position: 'D',
+        number: 4,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'Ibrahima Konate',
+        position: 'D',
+        number: 5,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'Konstantinos Tsimikas',
+        position: 'D',
+        number: 21,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'Andy Robertson',
+        position: 'D',
+        number: 26,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'Joel Matip',
+        position: 'D',
+        number: 32,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'Stefan Bajcetic',
+        position: 'D',
+        number: 43,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'Trent Alexander-Arnold',
+        position: 'D',
+        number: 66,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'Fabinho',
+        position: 'M',
+        number: 3,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'Thiago',
+        position: 'M',
+        number: 6,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'James Milner',
+        position: 'M',
+        number: 7,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'Naby Keita',
+        position: 'M',
+        number: 8,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'Jordan Henderson',
+        position: 'M',
+        number: 14,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'Alex Oxlade-Chamberlain',
+        position: 'M',
+        number: 15,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'Curtis Jones',
+        position: 'M',
+        number: 17,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'Harvey Elliot',
+        position: 'M',
+        number: 19,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'Fabio Carvalho',
+        position: 'M',
+        number: 28,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'Roberto Firmino',
+        position: 'F',
+        number: 9,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'Mohamed Salah',
+        position: 'F',
+        number: 11,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'Cody Gakpo',
+        position: 'F',
+        number: 18,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'Diogo Jota',
+        position: 'F',
+        number: 20,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'Luis Diaz',
+        position: 'F',
+        number: 23,
+        club: 'Liverpool'    
+    },
+    {
+        name: 'Darwin Nunez',
+        position: 'F',
+        number: 27,
+        club: 'Liverpool'    
+    }
+]
